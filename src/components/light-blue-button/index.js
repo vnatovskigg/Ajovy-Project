b23de972d3a6eb8e3189d9e6e11a927d97107344
vnatovskigg <49907900+vnatovskigg@users.noreply.github.com>
2020-09-13 13:36:22 +0300

    feat: light blue button

--- src/components/light-blue-button/index.js
+++ src/components/light-blue-button/index.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./index.module.css";
-import icon from "../../assets/interactive icons/icn_pi_default.svg";
+import icon from "../../assets/interactive icons/icn_mail_default.svg";
+import iconClicked from "../../assets/interactive icons/icn_mail_down.svg";
+import iconHover from "../../assets/interactive icons/icn_mail_hover.svg";
 
 const LightButton = () => {
+  const [hover, setHover] = useState(false);
+  const [clicked, setClicked] = useState(false);
+
+  const handleHover = () => {
+    setHover(true);
+  };
+
+  const handleHoverOut = () => {
+    setHover(false);
+    setClicked(false);
+  };
+
+  const handleClick = () => {
+    setClicked(true);
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.button}>
-        <img src={icon} className={styles.icon}></img>
-        <span className={styles.text}>SEE PRESCRIBING INFORMATION</span>
+      <div
+        className={hover ? styles["button-hover"] : styles.button}
+        onMouseEnter={handleHover}
+        onMouseLeave={handleHoverOut}
+        onClick={handleClick}
+      >
+        <img
+          src={hover ? (clicked ? iconClicked : iconHover) : icon}
+          className={styles.icon}
+        ></img>
+        <span className={hover ? styles["text-hover"] : styles.text}>
+          STAY CONNECTED
+        </span>
       </div>
     </div>
   );
